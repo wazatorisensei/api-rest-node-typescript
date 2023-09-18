b5a1530e678661183d17c7a1e3ad7ada65258ed4
Jan Yuzo Kohara <janyuzokohara@gmail.com>
2023-09-18 13:29:58 -0300

    FEAT: validator function middleware separate

--- src/server/controllers/cities/Create.ts
+++ src/server/controllers/cities/Create.ts
@@ -1,66 +1,26 @@
-import { Request, RequestHandler, Response } from 'express';
+import { Request, Response } from 'express';
 
 import { StatusCodes } from 'http-status-codes';
 
-import * as yup from 'yup';
+import { validation } from '../../shared/middleware';
 
-import { ICities } from './../../interfaces';
+import { ICities, IFilter } from '../../interfaces';
 
-const bodyValidation: yup.Schema<ICities> = yup.object().shape({
-  name: yup.string().required().min(3),
-  state: yup.string().required().min(3),
-});
+import { string, object } from 'yup';
 
-interface IFilter {
-  filter?: string;
-}
-
-const queryValidation: yup.Schema<IFilter> = yup.object().shape({
-  filter: yup.string().required().min(3),
-});
-
-// middleware
-export const createBodyValidator: RequestHandler = async (req, res, next) => {
-  try {
-    await bodyValidation.validate(req.body, {
-      abortEarly: false,
-    });
-    return next();
-  } catch (err) {
-    const yupError = err as yup.ValidationError;
-    const errors: Record<string, string> = {};
-    yupError.inner.forEach((error) => {
-      if (!error.path) return;
-      errors[error.path] = error.message;
-    });
-    return res.status(StatusCodes.BAD_REQUEST).json({
-      errors: {
-        default: errors || yupError.message,
-      },
-    });
-  }
-};
-
-export const createQueryValidator: RequestHandler = async (req, res, next) => {
-  try {
-    await queryValidation.validate(req.query, {
-      abortEarly: false,
-    });
-    return next();
-  } catch (err) {
-    const yupError = err as yup.ValidationError;
-    const errors: Record<string, string> = {};
-    yupError.inner.forEach((error) => {
-      if (!error.path) return;
-      errors[error.path] = error.message;
-    });
-    return res.status(StatusCodes.BAD_REQUEST).json({
-      errors: {
-        default: errors || yupError.message,
-      },
-    });
-  }
-};
+export const createValidation = validation((getSchema) => ({
+  body: getSchema<ICities>(
+    object().shape({
+      name: string().required().min(3),
+      state: string().required().min(3),
+    })
+  ),
+  query: getSchema<IFilter>(
+    object().shape({
+      filter: string().required().min(3),
+    })
+  ),
+}));
 
 export const create = async (req: Request<{}, {}, ICities>, res: Response) => {
   console.log(req.body);
