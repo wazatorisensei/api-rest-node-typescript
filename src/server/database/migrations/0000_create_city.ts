606bc430a93b1c91c385b09f4c3165a9d02fded9
Jan Yuzo Kohara <janyuzokohara@gmail.com>
2023-09-22 09:03:15 -0300

    REFACTOR: migrations to city

---
+++ src/server/database/migrations/0000_create_city.ts
@@ -0,0 +1,20 @@
+import { Knex } from 'knex';
+import { ETableNames } from '../ETableNames';
+
+export async function up(knex: Knex) {
+  return knex.schema
+    .createTable(ETableNames.city, (table) => {
+      table.bigIncrements('id').primary().index();
+      table.string('name', 150).index().notNullable();
+      table.comment('Table used to store cities in the system.');
+    })
+    .then(() => {
+      console.log(`# Created table ${ETableNames.city}`);
+    });
+}
+
+export async function down(knex: Knex) {
+  return knex.schema.dropTable(ETableNames.city).then(() => {
+    console.log(`# Dropped table ${ETableNames.city}`);
+  });
+}
