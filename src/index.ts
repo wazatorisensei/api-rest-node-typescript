5f75a855bdbd2ee6192e5858640568bcdaaf5397
Jan Yuzo Kohara <janyuzokohara@gmail.com>
2023-09-30 11:23:15 -0300

    FIX: resolve production problem

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,22 @@
 import { server } from './server/Server';
-server.listen(process.env.PORT || 3333, () => {
-  console.log(`App running on http://localhost:${process.env.PORT || 3333}`);
-});
+
+import { Knex } from './server/database/knex';
+
+const startServer = () => {
+  server.listen(process.env.PORT || 3333, () => {
+    console.log(`App running on http://localhost:${process.env.PORT || 3333}`);
+  });
+};
+
+if (process.env.IS_LOCALHOST !== 'true') {
+  // rodar migration em produção
+  Knex.migrate
+    .latest()
+    .then(() => {
+      startServer();
+    })
+    .catch(console.log);
+} else {
+  // rodar migration manualmente
+  startServer();
+}
