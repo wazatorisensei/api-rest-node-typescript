62382d906e5ac5609cf56e38e4a4c979f3dbc082
Jan Yuzo Kohara <janyuzokohara@gmail.com>
2023-10-06 10:22:19 -0300

    FEAT: terminal style

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,39 @@ import { server } from './server/Server';
 
 import { Knex } from './server/database/knex';
 
+export const reset = '\x1b[0m'; // reset before ❌
+export const b = '\x1b[36m'; // blue to info 🔵
+export const r = '\x1b[31m'; // red to fail 🔴
+export const g = '\x1b[32m'; // green to aprove 🟢
+
 const startServer = () => {
   server.listen(process.env.PORT || 3333, () => {
-    console.log(`App running on http://localhost:${process.env.PORT || 3333}`);
+    try {
+      console.log(
+        `🟢 ➡️ [${g}API${reset}] running on http://localhost:${
+          process.env.PORT || 3333
+        } or http://127.0.0.1:${process.env.PORT || 3333}`
+      );
+    } catch (error) {
+      console.error(
+        `🔴 ➡️ ${r}[API]${reset} fail to running on http://localhost:${
+          process.env.PORT || 3333
+        } or http://127.0.0.1:${process.env.PORT || 3333}`
+      );
+    }
   });
 };
 
 if (process.env.IS_LOCALHOST !== 'true') {
+  console.log('Running migrations !');
   // rodar migration em produção
   Knex.migrate
     .latest()
     .then(() => {
-      startServer();
+      Knex.seed
+        .run()
+        .then(() => startServer())
+        .catch(console.log);
     })
     .catch(console.log);
 } else {
